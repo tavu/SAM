72fa50c1c0f5cf5da86bd14d79e91fa9b447399d
root <root@debian>
2013-04-30 13:24:16 +0300

    Fixed bitrate.sh

--- utils/Bitrate/bitrate.sh
+++ utils/Bitrate/bitrate.sh
@@ -3,12 +3,12 @@
 log=bitrates.log
 wlan=ah
 location=$1
-currentTime=$(date +"%T")
-txPower=`iwconfig $wlan | grep Tx-Power | awk '{print $2}'`
-bitrate=`iwconfig $wlan | grep "Bit Rate" | awk '{ print $0 print $1}'`
+currentTime=$(date)
+txPower=`iwconfig $wlan | grep Tx-Power | awk '{print $4" "$5}'`
+bitrate=`iwconfig $wlan | grep "Bit Rate" | awk '{print $1" "$2" "$3}'`
 
 echo $currentTime >> $log
-echo $location >> $log
+echo Distance: $location >> $log
 echo $txPower >> $log
 echo $bitrate >> $log 
 
