21a64a3b0da2e392013908d4ef793d0aad71125b
Nikos <nl0st@nl0st-Aspire-5560.(none)>
2013-04-30 12:00:48 +0300

    Created bitrate measurement tool

--- utils/Bitrate/bitrate.sh
+++ utils/Bitrate/bitrate.sh
@@ -5,7 +5,7 @@ wlan=ah
 location=$1
 currentTime=$(date +"%T")
 txPower=`iwconfig $wlan | grep Tx-Power | awk '{print $2}'`
-bitrate=`iwconfig $wlan | grep Bit Rate | awk '{print $1}'`
+bitrate=`iwconfig $wlan | grep "Bit Rate" | awk '{print $1}'`
 
 echo $currentTime >> $log
 echo $location >> $log
