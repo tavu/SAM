add648abd958cb6bf0b035bf5ca828b57378dd00
root <root@nl0st-Aspire-5560.(none)>
2013-06-19 14:19:20 +0300

    Added peer.sh in utils which configures the wireless adapter of a peer

---
+++ utils/peer.sh
@@ -0,0 +1,47 @@
+wlan=ah
+name=test
+channel=11
+ip="10.0.0.2"
+
+phy=`iw list |grep Wiphy |awk '{print $2}'`
+
+if [ "$1" == "del" ]; then
+echo deleting interface
+        iw dev $wlan del
+	killall dhclient
+        exit
+fi
+
+#iw dev wlan1 del
+
+echo creating interface "$wlan"
+iw phy $phy interface add $wlan type managed
+echo ""
+
+
+#echo seting interface up
+ifconfig $wlan up
+#ifconfig $wlan $ip netmask 255.0.0.0
+#route add default gw 10.0.0.1
+echo ""
+
+echo setting channel to $channel
+#iw dev $wlan set channel $channel
+iwconfig $wlan channel $channel
+echo ""
+
+echo join the club
+iwconfig $wlan essid $name
+
+
+#echo gettin ip using dhcp
+dhclient $wlan
+
+
+
+
+#Start dhclient to get an address
+#dhclient $wlan
+#sudo ip link set $wlan up
+#sudo ip addr add 10.0.0.0/16 dev $wlan 
+#sudo iwconfig $wlan key 1234567890 
